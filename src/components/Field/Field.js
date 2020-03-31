81b505205ccbb51c609cbd8707e7274ea42f2406
pawelmaczka <pawel@maczka.it>
2020-03-31 16:19:52 +0200

    add more fields

--- src/components/Field/Field.js
+++ src/components/Field/Field.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components/macro';
+import styled, { css } from 'styled-components/macro';
 import PropTypes from 'prop-types';
 
 const StyledField = styled.div`
@@ -7,38 +7,47 @@ const StyledField = styled.div`
   height: 0.7em;
   display: inline-block;
   pointer-events: none;
-
   position: relative;
 
-  &:nth-child(4n - 2) {
+  &:nth-child(5n - 3) {
     transform: translate(-50%, -35%);
   }
 
-  &:nth-child(4n - 1) {
+  &:nth-child(5n - 2) {
     transform: translate(-100%, -70%);
   }
 
-  &:nth-child(4n) {
+  &:nth-child(5n - 1) {
     transform: translate(-150%, -105%);
   }
 
-  &:nth-child(n + 5) {
+  &:nth-child(5n) {
+    transform: translate(-200%, -140%);
+  }
+
+  &:nth-child(n + 6) {
     left: 50%;
     top: -35%;
   }
 
-  &:nth-child(n + 9) {
+  &:nth-child(n + 11) {
     left: 100%;
     top: -70%;
   }
 
-  &:nth-child(n + 13) {
+  &:nth-child(n + 16) {
     left: 150%;
     top: -105%;
   }
 
   svg {
     pointer-events: none;
+
+    ${({ grayed }) =>
+      grayed &&
+      css`
+        filter: grayscale(70%);
+      `}
 
     &:hover {
       filter: drop-shadow(5px 5px 10px rgba(221, 255, 83, 0.7));
@@ -52,15 +61,15 @@ const StyledField = styled.div`
 `;
 
 const Field = ({ svgComponents }) => {
-  const [level, setLevel] = React.useState(0);
+  const [level, setLevel] = React.useState(-1);
 
   const handleClick = React.useCallback(() => {
-    setLevel((lvl) => (lvl === 4 ? 0 : lvl + 1));
+    setLevel((lvl) => (lvl === 4 ? -1 : lvl + 1));
   }, [level]);
 
   React.useEffect(() => {
     const interval = setInterval(() => {
-      setLevel((lvl) => (lvl === 4 ? 0 : lvl + 1));
+      setLevel((lvl) => (lvl === 4 ? -1 : lvl + 1));
     }, 3000 + Math.random() * 60000);
 
     return () => {
@@ -68,10 +77,10 @@ const Field = ({ svgComponents }) => {
     };
   }, []);
 
-  const Image = svgComponents[level];
+  const Image = svgComponents[level < 0 ? 0 : level];
 
   return (
-    <StyledField>
+    <StyledField grayed={level === -1} readyToHarvest={level === 3}>
       <Image onClick={handleClick} />
     </StyledField>
   );
