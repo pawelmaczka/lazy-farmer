b60b1c4fefcebcda6d221835349adebd11f73490
pawelmaczka <pawel@maczka.it>
2020-04-01 20:33:13 +0200

    test firebase functions

--- functions/getDefaultFarm.js
+++ functions/getDefaultFarm.js
@@ -1,19 +1,127 @@
-const field = {
-  level: 0,
-  type: 'wheat',
-  productionStartTimestamp: null,
+const types = {
+  cabbage: 'cabbage',
+  carrot: 'carrot',
+  potato: 'potato',
+  pumpkin: 'pumpkin',
+  tomato: 'tomato',
 };
 
-const getDefaultFields = () =>
-  Array(6)
-    .fill(true)
-    .map((value, index) => ({
-      id: index,
-      ...field,
-    }));
+const defaultFields = [
+  {
+    id: 1,
+    type: types.cabbage,
+    productionStartTimestamp: null,
+  },
+  {
+    id: 2,
+    type: types.cabbage,
+    productionStartTimestamp: null,
+  },
+  {
+    id: 3,
+    type: types.cabbage,
+    productionStartTimestamp: null,
+  },
+  {
+    id: 4,
+    type: types.cabbage,
+    productionStartTimestamp: null,
+  },
+  {
+    id: 5,
+    type: types.cabbage,
+    productionStartTimestamp: null,
+  },
+  {
+    id: 6,
+    type: types.cabbage,
+    productionStartTimestamp: null,
+  },
+  {
+    id: 7,
+    type: types.cabbage,
+    productionStartTimestamp: null,
+  },
+  {
+    id: 8,
+    type: types.cabbage,
+    productionStartTimestamp: null,
+  },
+  {
+    id: 9,
+    type: types.cabbage,
+    productionStartTimestamp: null,
+  },
+  {
+    id: 10,
+    type: types.cabbage,
+    productionStartTimestamp: null,
+  },
+  {
+    id: 11,
+    type: types.cabbage,
+    productionStartTimestamp: null,
+  },
+  {
+    id: 12,
+    type: types.cabbage,
+    productionStartTimestamp: null,
+  },
+  {
+    id: 13,
+    type: types.cabbage,
+    productionStartTimestamp: null,
+  },
+  {
+    id: 14,
+    type: types.cabbage,
+    productionStartTimestamp: null,
+  },
+  {
+    id: 15,
+    type: types.cabbage,
+    productionStartTimestamp: null,
+  },
+  {
+    id: 16,
+    type: types.cabbage,
+    productionStartTimestamp: null,
+  },
+  {
+    id: 17,
+    type: types.cabbage,
+    productionStartTimestamp: null,
+  },
+  {
+    id: 18,
+    type: types.cabbage,
+    productionStartTimestamp: null,
+  },
+  {
+    id: 19,
+    type: types.cabbage,
+    productionStartTimestamp: null,
+  },
+  {
+    id: 20,
+    type: types.cabbage,
+    productionStartTimestamp: null,
+  },
+];
+
+const defaultResources = {
+  cabbage: 10,
+  carrot: 1,
+  potato: 1,
+  pumpkin: 1,
+  tomato: 1,
+};
+
+const getDefaultFields = () => defaultFields;
 
 const getDefaultFarm = () => ({
   avatar: null,
+  resources: defaultResources,
   fields: getDefaultFields(),
 });
 
--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
+// exports.helloWorld = functions.https.ondata((request, response) => {
 //   response.send('Hello from Firebase!');
 // });
 
@@ -22,4 +22,68 @@ exports.createNewFarm = functions.auth.user().onCreate((user) => {
 
 exports.deleteFarm = functions.auth.user().onDelete((user) => {
   return admin.firestore().collection('farms').doc(user.uid).delete();
+});
+
+exports.plant = functions.https.onCall(async ({ fieldId, type }, context) => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError(
+      'unauthenticated',
+      'Only authorized users can change their avatars'
+    );
+  }
+
+  if (!fieldId) {
+    throw new functions.https.HttpsError(
+      'invalid-argument',
+      'fieldId is required'
+    );
+  }
+  if (!type) {
+    throw new functions.https.HttpsError(
+      'invalid-argument',
+      'type is required'
+    );
+  }
+
+  const updatedField = {
+    id: fieldId,
+    type,
+    productionStartTimestamp: Date.now(),
+  };
+
+  const db = admin.firestore();
+  const batch = db.batch();
+
+  const farmRef = db.collection('farms').doc(context.auth.uid);
+  const farmData = await farmRef.get().then((doc) => doc.data());
+
+  const oldFieldValue = farmData.fields.find((field) => field.id === fieldId);
+  const resources = farmData.resources[type];
+
+  if (resources < 1) {
+    throw new functions.https.HttpsError(
+      'failed-precondition',
+      'to little resources'
+    );
+  }
+
+  if (!oldFieldValue) {
+    throw new functions.https.HttpsError(
+      'not-found',
+      `There is no field with given id: ${fieldId}`
+    );
+  }
+
+  batch.update(farmRef, {
+    fields: admin.firestore.FieldValue.arrayRemove(oldFieldValue),
+  });
+
+  batch.update(farmRef, {
+    fields: admin.firestore.FieldValue.arrayUnion(updatedField),
+    [`resources.${type}`]: resources - 1,
+  });
+
+  await batch.commit();
+
+  return 'ok';
 });
