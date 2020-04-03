03a52405c1ade97412675b860c00e2900db08012
pawelmaczka <pawel@maczka.it>
2020-04-03 13:45:03 +0200

    remove unused code

--- src/components/Field/Field.js
+++ src/components/Field/Field.js
@@ -9,20 +9,11 @@ import { getFieldLevel } from './helpers';
 import * as Styled from './Field.style';
 
 const plant = firebase.functions().httpsCallable('plant');
-// const TIME_TO_ROTTEN = 10800000;
-
-// const Loading = styled(CircularProgress)`
-//   position: absolute;
-//   top: 0.6em;
-//   left: 0.35em;
-//   z-index: 2;
-// `;
 
 const Field = ({ field: { id, plantedAtTimestamp, type } }) => {
   const [level, setLevel] = React.useState(
     getFieldLevel(type, plantedAtTimestamp)
   );
-  const [isPerformingAction, setIsPerformingAction] = React.useState(false);
 
   React.useEffect(() => {
     const interval = setInterval(() => {
@@ -33,17 +24,13 @@ const Field = ({ field: { id, plantedAtTimestamp, type } }) => {
   }, [plantedAtTimestamp]);
 
   const handleClick = React.useCallback(async () => {
-    await setIsPerformingAction(true);
     await plant({ fieldId: id, type });
-    await setIsPerformingAction(false);
   }, []);
 
   const Image = getFieldImage(type, level);
 
   return (
     <Styled.Field grayscale={level === 0}>
-      {/* {isPerformingAction && <Loading size={30} disableShrink />} */}
-      {/* <Loading size={30} disableShrink /> */}
       <Image onClick={handleClick} />
     </Styled.Field>
   );
