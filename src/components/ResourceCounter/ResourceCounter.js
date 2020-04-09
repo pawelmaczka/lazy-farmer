275f56e6d4999c916f7452f34bf7e85c6b564660
pawelmaczka <pawel@maczka.it>
2020-04-09 18:16:23 +0200

    moved images to assets folder

--- src/components/ResourceCounter/ResourceCounter.js
+++ src/components/ResourceCounter/ResourceCounter.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { ReactComponent as pumpkin } from './img/pumpkin_counter.svg';
-import { ReactComponent as cabbage } from './img/cabbage_counter.svg';
-import { ReactComponent as carrot } from './img/carrot_counter.svg';
-import { ReactComponent as potato } from './img/potato_counter.svg';
-import { ReactComponent as tomato } from './img/tomato_counter.svg';
+import { ReactComponent as pumpkin } from 'assets/images/pumpkin.svg';
+import { ReactComponent as cabbage } from 'assets/images/cabbage.svg';
+import { ReactComponent as carrot } from 'assets/images/carrot.svg';
+import { ReactComponent as potato } from 'assets/images/potato.svg';
+import { ReactComponent as tomato } from 'assets/images/tomato.svg';
 
 import * as Styled from './ResourceCounter.style';
 
